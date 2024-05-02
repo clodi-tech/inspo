4a6dce4e58e5ebcf3ece652974b656524c0df117
clodi <ciao@clodi.tech>
2024-05-02 18:08:42 +0200

    migration working

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,4 +2,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./db/schema.ts",
   out: "./db/drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: "",
+  },
 } satisfies Config;
