a4829294fc4b93bbff0d50bfa05acaebccb773cf
clodi <ciao@clodi.tech>
2024-05-02 12:28:49 +0200

    github provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { signIn } from "@/auth.ts";
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql);
@@ -8,6 +9,16 @@ export default function Home() {
   return (
     <main>
       <h1>{JSON.stringify(db)}</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github");
+        }}
+      >
+        <button type="submit" className="btn btn-info">
+          Signin with GitHub
+        </button>
+      </form>
     </main>
   );
 }
