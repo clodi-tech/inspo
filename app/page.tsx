e3b89187ac02044b275e3ab6b677424e688f979d
clodi <ciao@clodi.tech>
2024-05-02 11:45:00 +0200

    setup drizzle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql);
+
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1>{JSON.stringify(db)}</h1>
     </main>
   );
 }
