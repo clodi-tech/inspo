5dd27e17ecc362622d232b25d7bdf53d63a6436a
clodi <ciao@clodi.tech>
2024-05-03 10:55:08 +0200

    style

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,40 @@
 import { signIn, signOut } from "@/auth.ts";
 
+function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("github");
+      }}
+    >
+      <button type="submit" className="btn btn-primary">
+        Signin with GitHub
+      </button>
+    </form>
+  );
+}
+
+function SignOut() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <button type="submit" className="btn btn-secondary">
+        Sign Out
+      </button>
+    </form>
+  );
+}
+
 export default function Home() {
   return (
     <main>
-      {/* sign in button */}
-      <form
-        action={async () => {
-          "use server";
-          await signIn("github");
-        }}
-      >
-        <button type="submit" className="btn btn-info">
-          Signin with GitHub
-        </button>
-      </form>
-
-      {/* sign out button */}
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit" className="btn">
-          Sign Out
-        </button>
-      </form>
+      <SignIn />
+      <SignOut />
     </main>
   );
 }
