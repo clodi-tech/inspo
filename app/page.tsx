989e93aeaf22b9838c2f502eb863c7c157de672a
clodi <ciao@clodi.tech>
2024-05-03 10:21:35 +0200

    auth fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
 import { signIn } from "@/auth.ts";
-
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql);
 
 export default function Home() {
   return (
     <main>
-      <h1>{JSON.stringify(db)}</h1>
       <form
         action={async () => {
           "use server";
