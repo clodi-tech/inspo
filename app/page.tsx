3e541aa5914604d8b8f2e66e5abe7d9e685c8fb9
clodi <ciao@clodi.tech>
2024-05-09 16:36:49 +0200

    copy and bookmark setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import { Avatar, Logo, SignIn } from "@/ui/user";
 import Bookmark from "@/ui/bookmark";
+import Copy from "@/ui/copy";
 
 export default async function Home() {
   const session = await auth();
@@ -17,6 +18,7 @@ export default async function Home() {
     <main>
       <Logo />
       <Avatar user={session.user} />
+      <Copy />
       <Bookmark />
     </main>
   );
---
+++ ui/copy.tsx
@@ -0,0 +1,8 @@
+export default function Copy() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold">Title</h1>
+      <p>Banger headline</p>
+    </div>
+  );
+}
