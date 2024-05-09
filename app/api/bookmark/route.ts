e78535728508b3d8b6a8546c73150020ad2a264d
clodi <ciao@clodi.tech>
2024-05-09 18:35:51 +0200

    insert in db

--- app/api/bookmark/route.ts
+++ app/api/bookmark/route.ts
@@ -1,8 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { bookmarks } from "@/db/schema";
+import { auth } from "@/auth";
+
+const sql = neon(process.env.AUTH_DRIZZLE_URL!);
+const db = drizzle(sql);
 
 export async function POST(req: Request) {
   const body = await req.json();
   console.log("server create bookmark", body);
+
+  const session = await auth();
+  console.log("session", session);
+
+  const result = await db
+    .insert(bookmarks)
+    .values({ userId: session?.user?.id!, url: body.bookmark });
+  console.log("result", result);
 
   return NextResponse.json({ message: "Bookmark created" });
 }
