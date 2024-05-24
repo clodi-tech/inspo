3caeca79682d0337101135c39d4cc0c4f76d9f6f
clodi <ciao@clodi.tech>
2024-05-24 12:47:41 +0200

    setup form and server action

--- ui/card.tsx
+++ ui/card.tsx
@@ -1,29 +1,100 @@
 import Image from "next/image";
 import Delete from "./delete";
+import { saveCard } from "@/utils/actions";
 
 export default function Card({ content }: { content: any }) {
   return (
-    <div className="card max-w-sm bg-base-100 shadow-xl image-full">
-      <figure>
-        <Image
-          src={content.image}
-          alt="image"
-          style={{ width: "100%", height: "auto" }}
-          width={1200}
-          height={630}
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{content.title}</h2>
-        <p className="text-xs">{content.description}</p>
-        <div className="card-actions">
-          <div className="badge badge-accent">{content.tag}</div>
-          <a href={content.url} target="_blank" rel="noopener">
-            <div className="btn btn-primary btn-xs">Open</div>
-          </a>
-          <Delete id={content.id} />
+    <>
+      <div
+        className="card max-w-sm bg-base-100 shadow-xl image-full"
+        onClick={() => {
+          const modal = document.getElementById(
+            "edit_card"
+          ) as HTMLDialogElement;
+          if (modal) {
+            modal.showModal();
+          }
+        }}
+      >
+        <figure>
+          <Image
+            src={content.image}
+            alt="image"
+            style={{ width: "100%", height: "auto" }}
+            width={1200}
+            height={630}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{content.title}</h2>
+          <p className="text-xs">{content.description}</p>
+          <div className="card-actions">
+            <div className="badge badge-accent">{content.tag}</div>
+            <a href={content.url} target="_blank" rel="noopener">
+              <div className="btn btn-primary btn-xs">Open</div>
+            </a>
+            <Delete id={content.id} />
+          </div>
         </div>
       </div>
-    </div>
+      <dialog id="edit_card" className="modal">
+        <div className="modal-box">
+          <form action={saveCard}>
+            <label className="form-control w-full max-w-sm">
+              <div className="label">
+                <span className="label-text">Title</span>
+              </div>
+              <input
+                type="text"
+                defaultValue={content.title}
+                className="input input-bordered w-full max-w-sm"
+              />
+            </label>
+            <label className="form-control w-full max-w-sm">
+              <div className="label">
+                <span className="label-text">Description</span>
+              </div>
+              <textarea
+                defaultValue={content.description}
+                className="textarea textarea-bordered h-24"
+              />
+            </label>
+            <label className="form-control w-full max-w-sm">
+              <div className="label">
+                <span className="label-text">URL</span>
+              </div>
+              <input
+                type="text"
+                defaultValue={content.url}
+                className="input input-bordered w-full max-w-sm"
+              />
+            </label>
+            <label className="form-control w-full max-w-sm">
+              <div className="label">
+                <span className="label-text">Tag</span>
+              </div>
+              <input
+                type="text"
+                defaultValue={content.tag}
+                className="input input-bordered w-full max-w-sm"
+              />
+            </label>
+            <label className="form-control w-full max-w-sm">
+              <div className="label">
+                <span className="label-text">Image</span>
+              </div>
+              <input
+                type="text"
+                defaultValue={content.image}
+                className="input input-bordered w-full max-w-sm"
+              />
+            </label>
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
+          </form>
+        </div>
+      </dialog>
+    </>
   );
 }
---
+++ utils/actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export const saveCard = async () => {
+  console.log("saving card");
+};
